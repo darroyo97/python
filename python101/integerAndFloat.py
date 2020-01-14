6aece2d851eee910338d06b442e7503af94ee236
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 11:38:32 -0600

    adding lesson notes about operators

---
+++ python101/integerAndFloat.py
@@ -0,0 +1,23 @@
+# Integer
+
+print(6)
+
+# Float just means decimal point
+
+print(5.7)
+
+# mixed type will converted to a float
+
+print(8 + 1.2)
+
+# Python Operators
+# + Add
+# - Subtract
+# * Multiplication
+# / Division
+
+# Multiplication and division have a higher order than addition and subtraction.
+
+print(5//2)  # division    w/o    decimal
+print(5 % 2)  # modulus/remainder
+print(5**3)  # exponentiation
