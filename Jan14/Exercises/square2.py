40386674f96f09bbd7c530e8ff7409e85ddaf83e
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 16:31:36 -0600

    added solution with while and for loop

--- Jan14/Exercises/square2.py
+++ Jan14/Exercises/square2.py
@@ -1,6 +1,15 @@
 size = int(input("How big is the square? "))
 
-for i in range(size):
-    for i in range(size):
+# for i in range(size):
+#     for i in range(size):
+#         print("*", end=" ")
+#     print()
+
+i = 0
+while (i < size):
+    x = 0
+    while (x < size):
+        x = x + 1
         print("*", end=" ")
-    print()
+    i = i + 1
+    print(" ")
