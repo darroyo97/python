bdb7450a563733201502e2db43e6da4db3983209
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 13:37:43 -0600

    notes on conditions with examples to test

---
+++ python101/conditions.py
@@ -0,0 +1,31 @@
+# A conditional statement tells a program to execute different actions
+# depending on whether a condition is True or False.
+# -Conditional Operators
+# -Logical Operators
+# -the keywords “if, elif, else.”
+
+# Comparison Operators
+
+# Equal to: ==
+# Not equal to: !=
+# Less than: <
+# Greater than: >
+# Less than or equal to: <=
+# Greater than or equal to: >=
+
+# Logical Operators
+
+# 'and' operator – returns True if both the operands (right side and left side) are True
+# 'or' operator - returns True if either of the operand (right side or left side) is True
+# 'not' operator - returns True if operand is False
+# Examples
+print("test" != "testing")
+print("test" == 1)
+print(not (True and False))
+print(not (1 == 1 and 0 != 1))
+print(not (10 == 1 or 1000 == 1000))
+print(not (1 != 10 or 3 == 4))
+print(not ("testing" == "testing" and "Zed" == "Cool Guy"))
+print(1 == 1 and (not ("testing" == 1 or 1 == 0)))
+print("chunky" == "bacon" and (not (3 == 4 or 3 == 3)))
+print(3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
