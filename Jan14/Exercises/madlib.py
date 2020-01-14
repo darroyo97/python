441026f61a8b099f5adc2a7386e9f78e4446caf2
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 15:28:40 -0600

    madlib solution solved

---
+++ Jan14/Exercises/madlib.py
@@ -0,0 +1,8 @@
+print("Please fill in the blanks below:")
+
+print("___name___'s favorite subject in school is ___(subject)___.")
+
+name = input("What is name? ")
+subject = input("What is subject? ")
+madlib = f'{name}\'s favorite subject in school is {subject}'
+print(madlib)
