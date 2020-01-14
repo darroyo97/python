36351d5cde9c9e30a8817c8fa6bb8a2e1394a7c9
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 15:22:43 -0600

    solving solution 2

---
+++ Jan14/Exercises/hello_caps.py
@@ -0,0 +1,9 @@
+user_name = input("What is your name? ")
+caps_name = user_name.upper()
+length_of_name = len(user_name)
+
+greeting = f'HELLO {caps_name} !'
+greeting2 = f'YOUR NAME HAS {length_of_name} LETTERS IN IT! AWESOME'
+
+print(greeting)
+print(greeting2)
