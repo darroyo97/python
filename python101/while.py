e7a2b9a4deba2ada695db2c5e9425935fe2b4f38
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 15:04:43 -0600

    while loop notes and examples

---
+++ python101/while.py
@@ -0,0 +1,19 @@
+# count = 0
+# while count < 27:
+#     count += 1
+#     print("The count is", count)
+# print("Done")
+
+# answer = ''
+# while answer != 'when':
+#     answer = input('Say when: ')
+#     answer = answer.lower()
+# print("Cheese")
+
+# while break
+
+# while True:
+#     answer = input('Say when: ')
+#     if answer.lower() == 'when':
+#         break
+# print("Cheese")
