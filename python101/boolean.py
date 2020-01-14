dff99ab40e68bca4099a8c9894b64c0258c64d52
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 12:19:03 -0600

    added exmples of booleans

--- python101/boolean.py
+++ python101/boolean.py
@@ -3,3 +3,6 @@
 # They are used in comparisons to represent truth values.
 # In numeric contexts(for example, when used as the argument to an arithmetic operator),
 # they behave like the integers 0 and 1, respectively.
+
+print(10 == 9)
+print(10 < 9)
