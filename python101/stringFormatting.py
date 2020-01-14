9fd17b9d3e04e581f85292d4102365042138e085
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 11:57:11 -0600

    notes on different types of string formatting

---
+++ python101/stringFormatting.py
@@ -0,0 +1,16 @@
+# name = "Sunny"
+# name2 = "Juan"
+
+# output = "Hello {1} and {0}".format(name, name2)
+# # allows you to choose order {number}
+
+# print(output)
+
+
+# f-strings
+
+student1 = "john"
+student2 = "diego"
+
+output = f'hello  {student1}  and {student2}'
+print(output)
