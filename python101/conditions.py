39e37b23c07dd8ab124f7647d78050240e09f089
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 13:40:24 -0600

    adding notes on if condition

--- python101/conditions.py
+++ python101/conditions.py
@@ -19,13 +19,10 @@
 # 'or' operator - returns True if either of the operand (right side or left side) is True
 # 'not' operator - returns True if operand is False
 # Examples
-print("test" != "testing")
-print("test" == 1)
-print(not (True and False))
-print(not (1 == 1 and 0 != 1))
-print(not (10 == 1 or 1000 == 1000))
-print(not (1 != 10 or 3 == 4))
-print(not ("testing" == "testing" and "Zed" == "Cool Guy"))
-print(1 == 1 and (not ("testing" == 1 or 1 == 0)))
-print("chunky" == "bacon" and (not (3 == 4 or 3 == 3)))
-print(3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
+# print(not ("testing" == "testing" and "Zed" == "Cool Guy"))
+# print(1 == 1 and (not ("testing" == 1 or 1 == 0)))
+# print("chunky" == "bacon" and (not (3 == 4 or 3 == 3)))
+# print(3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
+
+
+# if
