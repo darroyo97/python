634d36f5b70b58549a8cbd929f161abfe9716f63
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 11:26:39 -0600

    learning strings and escape characters

--- python101/datatype.py
+++ python101/datatype.py
@@ -1,1 +1,35 @@
+# This is a string
+
+"I Am A String"
+
 print("I am a string")
+
+# print() allows to show on console
+
+
+# This is mutiple line string
+
+print("""
+I am a multiple
+line string
+get switchty 
+""")
+
+# Concatenating Strings: process of merging two strings into a single objective by using + operator
+
+print('Hello' + ' World')
+
+
+# Escaping characters using \ backslash
+# \ helps  with ' error to fix a string
+
+# print('Hello i'm a girl) <-error
+print('hello i\'m a girl')
+# \b backspace
+print('I am one line.\bI went back one space')
+# \t
+print('I am one line.\tI horizontal spacing')
+# \v
+print('I am one line.\vI horizontal spacing')
+# \n
+print('I am one line.\nI horizontal spacing')
