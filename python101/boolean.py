c71a0723afc529d6c927e7e0acc49e5f44d47679
Daniela <daniela.arroyo27@gmail.com>
2020-01-14 11:30:04 -0600

    add bool file

---
+++ python101/boolean.py
@@ -0,0 +1,5 @@
+# The Python type for storing true and false values is called bool.
+# Boolean values are the two constants False and True.
+# They are used in comparisons to represent truth values.
+# In numeric contexts(for example, when used as the argument to an arithmetic operator),
+# they behave like the integers 0 and 1, respectively.
