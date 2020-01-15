46f657bcaabcee17e520ea01293c65a6ac3ca44b
Daniela <daniela.arroyo27@gmail.com>
2020-01-15 13:32:37 -0600

    notes on tuples

---
+++ Jan15/LectureNotes/tuples.py
@@ -0,0 +1,15 @@
+# sequence of immutable Python objects
+# Tuples group one or more values of any type
+# parenthesis are optional
+# Tuples are Immutable
+
+
+# myTuble = (1, 2, "one", "two")
+# # myTuble[]
+# print(myTuble)
+
+
+num = (1, 2, 3)
+one, two, three = num
+
+print(one)
