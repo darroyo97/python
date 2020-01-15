8de5d0297f3726c2f4947986a9b814a47ffc69b5
Daniela <daniela.arroyo27@gmail.com>
2020-01-15 10:23:04 -0600

    notes on addint to a list 3 different ways

---
+++ Jan15/LectureNotes/list_while.py
@@ -0,0 +1,34 @@
+# todos = ["cook breakfest", "feed dogs", "put gas",
+#          "shop for groceries", "go to school"]
+# print(todos)
+
+# todos.append("take a nap")
+# todos.append("workout")
+# print(todos)
+
+
+# This while loop allows to show it as a list with number
+# index = 0
+# while index < len(todos):
+#     #print(f'{index}: {todos[index]}')
+#     print(f'{index + 1 }: {todos[index]}')
+#     index += 1
+
+
+# alternative you can use the concatenation to combine two list like append +
+# todos = ["cook breakfest", "feed dogs", "put gas",
+#          "shop for groceries", "go to school"]
+# # print(todos)
+
+# todos = todos + ["take a nap", "go to sleep "]
+# print(todos)
+
+
+# Extend() Method
+
+todos = ["pet the cat", "go to work", "shop for groceries",
+         "go home", "feed the cat"]
+
+todos.extend(["binge watch a show", "go to sleep"])
+
+print(todos)
