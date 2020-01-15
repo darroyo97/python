9f4fb9959b5dc1f08ade8da0139d69174fc368f4
Daniela <daniela.arroyo27@gmail.com>
2020-01-15 10:33:16 -0600

    notes on removing items from a list

---
+++ Jan15/LectureNotes/list_remove.py
@@ -0,0 +1,11 @@
+# Deleted items
+
+# use del keywords
+
+todos = ["pet the cat", "go to work",
+         "shop for groceries", "go home", "feed the cat"]
+
+# del todos[0]  # shoudl remove first one
+
+del todos[1:3]  # Remove items at index 1 up but not including index 3
+print(todos)
