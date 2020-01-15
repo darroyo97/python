a944aeeab99efc507df98baacaf81d504fab69b3
Daniela <daniela.arroyo27@gmail.com>
2020-01-15 10:01:29 -0600

    further notes on list with examples

--- Jan15/LectureNotes/list.py
+++ Jan15/LectureNotes/list.py
@@ -1,3 +1,26 @@
+# List a value that contains multiple values in an ordered sequence
+
+# Lists are zero-based, which means the first index,
+# the first element in the list, is considered to be at index 0, not index 1
+
 food = ["pizza", "chicken soup", "tacos", "eggroll", "gorditas", "lasagna"]
 
 print(food[2])
+
+# len()
+# get the number of elements
+
+print(len(food))
+
+
+# Modtify a list, add, remove, replace
+
+# Replace value of tacos into hotpocket
+food[2] = "hotpocket"
+
+print(food)
+
+# Append add items, adds to end of the list
+
+food.append("tamales")
+print(food)
