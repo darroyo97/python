0115318e7887b78b140dcd867ac8c93d32369245
Daniela <daniela.arroyo27@gmail.com>
2020-01-15 13:24:06 -0600

    notes on strings as list

---
+++ Jan15/LectureNotes/list_methods.py
@@ -0,0 +1,37 @@
+# List Insert as specific location
+numbers = [13, 342, 3, 44, 5]
+# print(numbers)
+# numbers.insert(3, 6)
+# new_num_list = numbers[0:2]
+# numbers.insert(3, new_num_list)
+# print(numbers)
+
+
+# List pop removes an item form the end of lost
+
+# numbers.pop()
+
+# List index returns index of the item in the list
+
+# print(numbers.index(4))
+
+# numbers.append(5)
+# print(numbers)
+# newNum = numbers.copy()
+# print(newNum)
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+# index = 0
+# while index < len(alphabet):
+#     print(alphabet[index])
+#     index += 1
+
+
+# turn string into list
+alphalist = list(alphabet)
+print(alphalist)
+
+
+# join to reconnect
+alphabet = "\n".join(alphalist)
+print(alphabet)
