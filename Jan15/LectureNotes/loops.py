3747f1fc95c249e8c4f87c4186e6d6d792af408f
Daniela <daniela.arroyo27@gmail.com>
2020-01-15 14:02:42 -0600

    noted on for loops

---
+++ Jan15/LectureNotes/loops.py
@@ -0,0 +1,16 @@
+# There tow types of loops in python the for loop and the while loop
+
+#
+# names = ['George', 'W', 'Bush']
+
+# for num in range(1, 100, 2):
+#     print(num)
+
+
+# for num in range(1, 100, 2):
+#     print(num)
+
+
+for num in range(1, 12):
+    finalNum = num * 2
+    print(finalNum)
