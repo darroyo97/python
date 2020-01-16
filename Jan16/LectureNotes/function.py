90ab19b9422484cdff57d6cacbf123c3a9f8e8ea
Daniela <daniela.arroyo27@gmail.com>
2020-01-16 13:35:16 -0600

    notes on function and example on how they are more effective

---
+++ Jan16/LectureNotes/function.py
@@ -0,0 +1,56 @@
+# Functions
+
+# def is the keyword for a function
+
+
+# Structure of a function
+# def nameoffunction(paramater or paramaters):
+# body
+
+# def greeting():
+#     print("Hello World")
+
+
+# # if you run this it does not run anything,
+# # you must call the function
+# greeting()
+
+# print("Day 1: Students in SRE class")
+# print("lecture: git 101")
+# print("Shu")
+# print("Thomas")
+# print("Gustavo")
+# print("Alim")
+# print("Day 2: Students in SRE class")
+# print("lecture: git 102")
+# print("Shu")
+# print("Thomas")
+# print("Gustavo")
+# print("Alim")
+# print("Day 3: Students in SRE class")
+# print("lecture: python 101")
+# print("Shu")
+# print("Thomas")
+# print("Gustavo")
+# print("Alim")
+
+
+# not functanial beacause we could get a mistake if we change a name we woudl have to
+# change it in every single line when we could just make the name into a function
+
+def names():
+    print("Shu Pei")
+    print("Thomas")
+    print("Gustavo Fette")
+    print("Alim")
+
+
+print("Day 1: Students in SRE class")
+print("lecture: git 101")
+names()
+print("Day 2: Students in SRE class")
+print("lecture: git 102")
+names()
+print("Day 3: Students in SRE class")
+print("lecture: python 101")
+names()
