7220082c0298cd989273aeb02870734a9ab73161
Daniela <daniela.arroyo27@gmail.com>
2020-01-15 23:12:50 -0600

    added same code with neg nums

--- Jan15/Exercises/small/pos_num.py
+++ Jan15/Exercises/small/pos_num.py
@@ -1,7 +1,16 @@
-list1 = [9, 23, -9, -56, 7, -8, 12]
-list2 = []
-for pos_num in list1:
-    if pos_num > 0:
-        list2.append(pos_num)
+# list1 = [9, 23, -9, -56, 7, -8, 12]
+# list2 = []
+# for pos_num in list1:
+#     if pos_num > 0:
+#         list2.append(pos_num)
 
+# print(list2)
+
+# tried neg nums
+list1 = [4, -7, -3, 12, 9, -8]
+list2 = []
+
+for neg_num in list1:
+    if neg_num < 0:
+        list2.append(neg_num)
 print(list2)
