9326429f5c4d43e6d415c36367fedbee415c201d
Daniela <daniela.arroyo27@gmail.com>
2020-01-16 13:42:41 -0600

    example on how you can call a function multiple times

--- Jan16/LectureNotes/function.py
+++ Jan16/LectureNotes/function.py
@@ -38,19 +38,39 @@
 # not functanial beacause we could get a mistake if we change a name we woudl have to
 # change it in every single line when we could just make the name into a function
 
-def names():
-    print("Shu Pei")
-    print("Thomas")
-    print("Gustavo Fette")
-    print("Alim")
+# def names():
+#     print("Shu Pei")
+#     print("Thomas")
+#     print("Gustavo Fette")
+#     print("Alim")
+
+# print("Day 1: Students in SRE class")
+# print("lecture: git 101")
+# names()
+# print("Day 2: Students in SRE class")
+# print("lecture: git 102")
+# names()
+# print("Day 3: Students in SRE class")
+# print("lecture: python 101")
+# names()
+
+# we cam put a loop inside a fucntion
+
+# def myFunction():
+#     for i in range(10):
+#         print('Hi')
 
 
-print("Day 1: Students in SRE class")
-print("lecture: git 101")
-names()
-print("Day 2: Students in SRE class")
-print("lecture: git 102")
-names()
-print("Day 3: Students in SRE class")
-print("lecture: python 101")
-names()
+# myFunction()
+
+# def getGroceries():
+#     print('milk')
+#     print('flour')
+#     print('sugar')
+#     print('butter')
+#     print()
+
+
+# getGroceries()
+# getGroceries()
+# getGroceries()
