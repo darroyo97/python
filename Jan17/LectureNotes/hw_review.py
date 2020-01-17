bd841800673622819657b32c4e01268aaf95919c
Daniela <daniela.arroyo27@gmail.com>
2020-01-17 13:07:25 -0600

    solution not solved yet

---
+++ Jan17/LectureNotes/hw_review.py
@@ -0,0 +1,15 @@
+# list1 = [5, 8, 3, 4, 2]
+
+# smallest = list1[0]
+
+# for num in list1:
+#     if (num < smallest):
+#         smallest = num
+
+# print(smallest)
+
+# myList = [1,2,3,4,5,6,7,8,9]
+# def FindMin():
+#     number = min(myList)
+#     return number
+# print("the smallest is:", FindMin())
