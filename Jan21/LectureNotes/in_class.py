b5509233ee2354f0074fa16a7060b0e806a51ccf
Daniela <daniela.arroyo27@gmail.com>
2020-01-21 13:49:06 -0600

    in class work completed

---
+++ Jan21/LectureNotes/in_class.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+    def greet(self, other_person):
+        print('Hello {}, I am {}!'.format(other_person.name, self.name))
+
+    def contact_info(self):
+        print(
+            f'Hello my name is {self.name} and my email is {self.email} also  \t my number is {self.phone}')
+        # print(self.email)
+        # print(self.phone)
+
+
+sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
+jordan = Person("Jordan", "jordan@aol.com", "495-586-3455")
+
+
+sonny.greet(jordan)
+jordan.greet(sonny)
+sonny.contact_info()
+jordan.contact_info()
