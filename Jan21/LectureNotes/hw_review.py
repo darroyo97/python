218f4e01f5787537918d7c86e79ca9a8bb73ae6c
Daniela <daniela.arroyo27@gmail.com>
2020-01-21 09:44:09 -0600

    word summary notes

--- Jan21/LectureNotes/hw_review.py
+++ Jan21/LectureNotes/hw_review.py
@@ -1,12 +1,23 @@
-# LetterSummary
+# # LetterSummary
 
 
-word = input("Enter a word >>")
+# word = input("Enter a word >>")
 
-empty_dic = {}
+# empty_dic = {}
 
-for letters in word:
-    if letters not in empty_dic:
-        empty_dic[letters] = 0
-    empty_dic[letters] += 1
-print(empty_dic)
+# for letters in word:
+#     if letters not in empty_dic:
+#         empty_dic[letters] = 0
+#     empty_dic[letters] += 1
+# print(empty_dic)
+
+# word summary
+# 2.
+sent_histogram = {}
+entered_sentence = input("Please enter a sentence... ")
+sent_to_words = entered_sentence.split()
+for repeats in sent_to_words:
+    if repeats not in sent_histogram:
+        sent_histogram[repeats] = 0
+    sent_histogram[repeats] += 1
+print(sent_histogram)
