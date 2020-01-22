248bbfdabc962fe05478f11395f1b09ce4e150a0
Daniela <daniela.arroyo27@gmail.com>
2020-01-22 11:52:16 -0600

    starting point of main menu option

--- Jan22/LectureNotes/in_class_app.py
+++ Jan22/LectureNotes/in_class_app.py
@@ -1,10 +1,10 @@
 # Bank Account App
 class AccountHolder:
     def __init__(self, fname, lname, mname, type, status, balance):
-        self.first_name = fname
-        self.last_name = lname
-        self.middle_name = mname
-        self.account_type = type
+        self.fname = fname
+        self.lname = lname
+        self.mname = mname
+        self.type = type
         self.status = status
         self.balance = balance
 
@@ -18,14 +18,45 @@ class Bank:
     def open_new_account(self, fname, lname, mname, type, status, balance):
         if balance >= 100:
             # create an account holder
+            account = AccountHolder(fname, lname, mname, type, status, balance)
+            self.accounts.append(account)
+            return print(f'{type } account has been open for {fname} {mname} {lname} with a balance of {balance}')
             # then we going to store it into our list of accounts
             # return a message to user if it was succesful
         else:
-            # return of insufficient deposit amount
+            return "Insufficient Funds, to open an account myst have deposit of at least $100"
 
             # definition of main that includes a while loop with menu of things user wants to do
             # main()
+    def show_members(self):
+        for members in self.accounts:
+            print(
+                f'{members.type} account \n Name: {members.fname} {members.mname} {members.lname} \n  Balance: {members.balance}')
 
 
-wellsfargo = Bank("wells fargo", "123 Sesame St.")
-wellsfargo.open_new_account("Veronica", "Leno", "Celeste", "Checking", 102)
+def main():
+    wellsfargo = Bank("wells fargo", "123 Sesame St.")
+    action = 1
+    while action != 6:
+        print("1. Create An Account: ")
+        print("2. Print List Of All Account Holders: ")
+        print("6. Exit Application: ")
+
+        action = int(input("What would you like to do? "))
+
+        if (action == 1):
+            fname = input("What is the first name? ")
+            mname = input("What is the middle name? ")
+            lname = input("What is the last name? ")
+            type = input("What would you like to open? (Checking or Saving)")
+            balance = int(input("Amount you wish to deposit? "))
+
+            wellsfargo.open_new_account(
+                fname, lname, mname, type, "open", balance)
+        elif (action == 2):
+            wellsfargo.show_members()
+        elif (action == 6):
+            break
+
+
+main()
