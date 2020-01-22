ef38a907c4abbcea1e42b2ba925f74df17c23ef9
Daniela <daniela.arroyo27@gmail.com>
2020-01-21 22:34:05 -0600

    hw assignment completed

--- Jan21/LectureNotes/in_class.py
+++ Jan21/LectureNotes/in_class.py
@@ -3,9 +3,24 @@ class Person:
         self.name = name
         self.email = email
         self.phone = phone
+        self.friends = []
+        self.count = 0
+
+    def greeting(self):
+        print(f'Hello {self.name} !')
+        self.count += 1
+
+    def printCount(self):
+        print(self.count)
 
     def greet(self, other_person):
         print('Hello {}, I am {}!'.format(other_person.name, self.name))
+
+    def friend_list(self, friend_name):
+        self.friend_name = friend_name
+        self.friends.append(friend_name)
+        print(self.friends)
+        print(len(self.friends))
 
     def contact_info(self):
         print(
@@ -16,9 +31,14 @@ class Person:
 
 sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
 jordan = Person("Jordan", "jordan@aol.com", "495-586-3455")
+# sonny.friend_list("jordan")
+# sonny.friend_list("joe")
 
-
-sonny.greet(jordan)
-jordan.greet(sonny)
-sonny.contact_info()
-jordan.contact_info()
+sonny.greeting()
+sonny.greeting()
+sonny.greeting()
+sonny.printCount()
+# sonny.greet(jordan)
+# jordan.greet(sonny)
+# sonny.contact_info()
+# jordan.contact_info()
