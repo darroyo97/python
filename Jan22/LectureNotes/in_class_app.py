e1b8b4db3de366726965258b9f63ef7a2dc0cd18
Daniela <daniela.arroyo27@gmail.com>
2020-01-22 11:05:26 -0600

    starting pint notes on functionality

--- Jan22/LectureNotes/in_class_app.py
+++ Jan22/LectureNotes/in_class_app.py
@@ -1,5 +1,5 @@
 # Bank Account App
-class Account Holder:
+class AccountHolder:
     def __init__(self, fname, lname, mname, type, status, balance):
         self.first_name = fname
         self.last_name = lname
@@ -14,3 +14,18 @@ class Bank:
         self.name = name
         self.address = address
         self.accounts = []
+
+    def open_new_account(self, fname, lname, mname, type, status, balance):
+        if balance >= 100:
+            # create an account holder
+            # then we going to store it into our list of accounts
+            # return a message to user if it was succesful
+        else:
+            # return of insufficient deposit amount
+
+            # definition of main that includes a while loop with menu of things user wants to do
+            # main()
+
+
+wellsfargo = Bank("wells fargo", "123 Sesame St.")
+wellsfargo.open_new_account("Veronica", "Leno", "Celeste", "Checking", 102)
